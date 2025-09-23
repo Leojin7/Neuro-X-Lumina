01fda213e75ba81c4d619ab1f60b5628102cf3f4
Dev Ruhela <leo18jin@gmail.com>
2025-09-23 23:36:59 +0530

    Update usePortfolioStore.ts

--- stores/usePortfolioStore.ts
+++ stores/usePortfolioStore.ts
@@ -6,6 +6,7 @@ import { generateResumeContent } from '../services/geminiService';
 interface PortfolioState {
     professionalTitle: string;
     bio: string;
+    
     socialLinks: {
         github: string;
         linkedin: string;
