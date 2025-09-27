8eefe00cc2cbd0a01b72657e6541220533d18d3e
Dev Ruhela <leo18jin@gmail.com>
2025-09-27 22:52:26 +0530

    Update ResumeGenerator.tsx

--- components/portfolio/ResumeGenerator.tsx
+++ components/portfolio/ResumeGenerator.tsx
@@ -6,6 +6,7 @@ import Card from '../Card';
 import ResumeModal from './ResumeModal';
 import { usePortfolioStore } from '../../stores/usePortfolioStore';
 const ResumeGenerator = () => {
+    
     const [isGenerating, setIsGenerating] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const generateAndSetResume = usePortfolioStore(state => state.generateAndSetResume);
